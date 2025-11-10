a263a0d4cbcfbe08d1a9104932f5c9ee5b98cdba
Mehemmed Emciyev <emciyevmaqa41@gmail.com>
2025-11-10 16:19:14 +0400

    Add ProductSaleAddTypes.ts - Generated from Canvas

---
+++ img/ProductSaleAddTypes.ts
@@ -0,0 +1,65 @@
+export interface ProductSaleAddProps {
+  // Define props here if needed
+}
+
+export interface ProductSaleAddState {
+  // Define state variables and their types here
+  tənzimIkonu: string;
+  ataAdı: string;
+  birləşənMiqdar: string;
+  təhvilTarixi: string;
+  məhsul: string;
+  əməliyyatNömrə: string;
+  satışMəbləği: string;
+  əlavəMiqdar: string;
+  növ: string;
+  satışMiqdarı: string;
+  güzəştFaizi: string;
+  əlaqəliƏsasVəsait: string;
+  filial: string;
+  məzmunSeçimi: string;
+  dəyiş: string;
+  məbləğAZN: string;
+  layihəMeneceri: string;
+  əlavəEt: string;
+  soyad: string;
+  əlaqəNömrəsi1: string;
+  ödənişNövü: string;
+  satışNövü: string;
+  mehsulKodu: string;
+  yekunQiymət: string;
+  faylƏlavəEt: string;
+  ədvMəbləğ: string;
+  vahid: string;
+  prioritet: string;
+  birləşənMəhsullar: string;
+  sil: string;
+  anbar: string;
+  hıdden: string;
+  mühasib: string;
+  qiymətAZN: string;
+  yekunMəbləğ: string;
+  əlavəVahid: string;
+  əməliyyatTarixiSaatı: string;
+  şirkət: string;
+  əməliyyatStatus: string;
+  dərəcə: string;
+  valyuta: string;
+  əsasMəbləğ: string;
+  kontragentƏtraflı: string;
+  silinənMiqdar: string;
+  yeniMəzmun: string;
+  ceki: string;
+  isChecked: string;
+  əlaqəNömrəsi2: string;
+  güzəştMəbləği: string;
+  açıqlama: string;
+  məhsulKodu2: string;
+  altMəhsullar: string;
+  qeyd: string;
+  satışQiyməti: string;
+  əlavəVahid2: string;
+  zəmanət: string;
+  daxilEdən: string;
+  qalıqMiqdar: string;
+}
