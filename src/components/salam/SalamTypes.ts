88bff5430e8115b96a3a49d14ae0c4c90af530c3
Mehemmed Emciyev <emciyevmaqa41@gmail.com>
2025-11-12 19:36:39 +0400

    Add src/components/salam/SalamTypes.ts - Generated from Canvas

---
+++ src/components/salam/SalamTypes.ts
@@ -0,0 +1,14 @@
+export interface SalamProps {
+  description: string;
+}
+
+export interface InputProps {
+  id: string;
+  cellNo: string;
+  content: string;
+  inputType: string;
+  hasLabel: boolean;
+  inputName: string;
+  componentType: string;
+  order: number;
+}
